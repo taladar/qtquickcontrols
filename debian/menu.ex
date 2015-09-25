f2870df58adce66a049202b7983d207d8c067c14
Matthias Hörmann <hoermann@saltation.de>
2015-09-25 14:33:54 +0200

    remove examples from debian/

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(qtquickcontrols):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="qtquickcontrols" command="/usr/bin/qtquickcontrols"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libqtquickcontrols 4.99 qtquickcontrols (>> 4.99-0), qtquickcontrols (<< 4.99-99)
